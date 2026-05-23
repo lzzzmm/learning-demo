30e5e2cebc7f33a52b14a8696e837fbe435375d7
CEDAR.L <cedar.lin@sjfood.com>
2026-05-23 07:31:41 +0000

    f

---
+++ AI/fastapi-langchain-demo/Api/route.py
@@ -0,0 +1,19 @@
+﻿import code
+from hashlib import new
+
+from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
+# 根路由
+@router.get("/")
+def read_root():
+    obj =  FastApiBaseModel(code = 200, message = "success", data = None)
+    return obj
+
+
+
+class FastApiBaseModel(BaseModel):
+    code: int
+    message: str
+    data: object
